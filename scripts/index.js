5bedf8aa42cb499d6eddfe83bbe8d07dcea1f1fb
arlekin1208 <arlekin12089@gmail.com>
2021-12-04 09:11:36 +0100

    added active class for sub-pages

--- scripts/index.js
+++ scripts/index.js
@@ -45,3 +45,15 @@ function Menu () {
 }
 
 Menu();
+
+//Active sub-pages
+( function () {
+	var current = location.pathname.split( '/' )[1];
+	if ( current === "" ) return;
+	var menuItems = document.querySelectorAll( '.main-menu li a' );
+	for ( var i = 0, len = menuItems.length; i < len; i++ ) {
+		if ( menuItems[i].getAttribute( "href" ).indexOf( current ) !== -1 ) {
+			menuItems[i].className += " is-active";
+		}
+	}
+} )();
